646731ee18a12e223a69efed0444f9099db81de6
Cadu Rocha <carlos.rocha@infraone.com.br>
2024-08-27 17:05:55 -0300

    Update MyourHead.js
    Metas

--- src/layouts/MyourHead.js
+++ src/layouts/MyourHead.js
@@ -2,9 +2,14 @@ import Head from "next/head";
 const MyourHead = () => {
   return (
     <Head>
-      <title>Carlos Eduardo Rocha (Cadu Rocha) - Currículo Product Owner</title>
+      <title>Cadu Rocha - Profissional de TI</title>
 
-    
+      <meta name="description" content="Sou Cadu, especialista em tecnologia, marketing digital e gestão de projetos. Com 15 anos de experiência, ajudo empresas a crescerem com estratégias digitais, desenvolvimento web e liderança de equipes. Explore meu portfólio e veja como posso impulsionar seu próximo projeto." />
+      <meta name="author" content="Cadu Rocha" />
+      <meta name="robots" content="index, follow" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
     </Head>
   );
 };
